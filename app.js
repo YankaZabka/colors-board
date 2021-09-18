e4dae64a4ad6e3faf8443ae61f20b14c87536604
YankaZabka <yanka.zabka@gmail.com>
2021-09-18 16:38:21 +0300

    Finished

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board')
+const colors = ['#009900', '#0099CC', '#663399', '#FF0066', '#FFFF00', '#3300FF', '#33FFFF']
+const SQUARES_NUMBER = 648
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+    square.addEventListener('mouseleave',removeColor)
+
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor () {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
